ead99adc84d0927f2834a137879db2c8e37254f6
robertluciand <65246056+robertluciand@users.noreply.github.com>
2020-10-21 00:08:46 +0300

    Tema 4

---
+++ Duicu Robert Lucian/L04/QuestionDomain/CreateQuestionWorkflow/CreateQuestionCmd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QuestionDomain.CreateQuestionWorkflow
+{
+    public struct CreateQuestionCmd
+    {
+        [Required]
+        public string Title { get; private set; }
+        [Required]
+        public string Content { get; private set; }
+
+        public CreateQuestionCmd(string Title, string Content)
+        {
+            this.Title = Title;
+            this.Content = Content;
+        }
+    }
+}
---
+++ Duicu Robert Lucian/L04/QuestionDomain/CreateQuestionWorkflow/CreateQuestionResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace QuestionDomain.CreateQuestionWorkflow
+{
+    [AsChoice]
+    public static partial class CreateQuestionResult
+    {
+        public interface ICreateQuestionResult { }
+
+        public class QuestionCreated : ICreateQuestionResult
+        {
+            public string Title { get; private set; }
+            public string Content{ get; private set; }
+
+            public QuestionCreated(string Titlu, string Content)
+            {
+                this.Title = Title;
+                this.Content = Content;
+
+            }
+        }
+
+        public class QuestionNotCreated : ICreateQuestionResult
+        {
+            public string Reason { get; set; }
+
+            public QuestionNotCreated(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        public class QuestionValidationFailed : ICreateQuestionResult
+        {
+            public IEnumerable<string> ValidationErrors { get; private set; }
+
+            public QuestionValidationFailed(IEnumerable<string> errors)
+            {
+                ValidationErrors = errors.AsEnumerable();
+            }
+        }
+    }
+}
